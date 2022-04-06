25685cc8b66771bb8220c0c3e6472e9556f006cf
Daniel Trondoli <daniel.trondoli@monitoratec.com.br>
2022-04-06 17:20:03 -0300

    * add project kafka consumer to processes transactions *add mutation to att account balance

--- graphql/src/main/java/com/dtrondoli/service/AccountService.java
+++ graphql/src/main/java/com/dtrondoli/service/AccountService.java
@@ -1,8 +1,10 @@
 package com.dtrondoli.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.query.JpaCountQueryCreator;
 import org.springframework.stereotype.Component;
 
 import com.dtrondoli.domain.Account;
@@ -10,14 +12,14 @@ import com.dtrondoli.repository.AccountRepository;
 
 @Component
 public class AccountService {
-	
+
 	@Autowired
 	private AccountRepository repo;
-	
+
 	public Account findById(Long id) {
 		return repo.findById(id).orElse(null);
 	}
-	
+
 	public List<Account> findAll() {
 		return repo.findAll();
 	}
@@ -25,12 +27,24 @@ public class AccountService {
 	public Account save(Account c) {
 		return repo.save(c);
 	}
-	
+
+	public Account attBalance(Long id, float newBalance) {
+
+		Optional<Account> opt = repo.findById(id);
+		if (opt.isPresent()) {
+			Account ac = opt.get();
+			ac.setBalance(newBalance);
+			return repo.save(ac);
+		}
+		
+		return null;
+	}
+
 	public boolean delete(Long id) {
-		if(repo.findById(id).isPresent()) {
+		if (repo.findById(id).isPresent()) {
 			repo.deleteById(id);
 			return true;
-		}		
+		}
 		return false;
 	}
 }
